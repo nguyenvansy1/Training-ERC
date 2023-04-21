013f00b48564510fefbbcb103c753f0cc933f136
nguyenvansy1 <ashewarakill2@gmail.com>
2023-04-21 14:09:37 +0700

    Done

---
+++ C#/Advance Training/.NET/Assignment2/Program.cs
@@ -0,0 +1,50 @@
+using Microsoft.EntityFrameworkCore;
+using Assignment2.Models;
+var builder = WebApplication.CreateBuilder(args);
+
+// Add Cors
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("MyPolicy", builder =>
+    {
+        builder.WithOrigins("*")
+               .WithMethods("GET", "POST", "PUT", "DELETE")
+               .WithHeaders("*");
+    });
+});
+
+// Add services to the container.
+builder.Services.AddControllers();
+
+// Add database context
+builder.Services.AddDbContextPool<ToDoContext>(
+    (serviceProvider, options) => options.UseMySql(
+        serviceProvider.GetRequiredService<IConfiguration>().GetConnectionString("DBConn"),
+        new MySqlServerVersion(new Version(8, 0, 23))
+    )
+);
+
+// Add Swagger/OpenAPI services
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+// Use CORS
+app.UseCors("MyPolicy");
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
